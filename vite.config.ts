0d08c7333342f86161d8186da91c79852c1e5641
stherzada <sthefanypequeno@gmail.com>
2025-06-28 20:10:36 -0300

    chore: Remove compression plugin from Vite configuration; update various image assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
-import compression from 'vite-plugin-compression'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    compression({
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
-    compression({
-      algorithm: 'brotliCompress',
-      ext: '.br',
-    }),
   ],
   resolve: {
     alias: {
